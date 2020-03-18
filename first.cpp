7663f6bce9a67e199daa9eea8ad8200f531f7843
Yehor Bilenko <lightdevgames@gmail.com>
2020-03-18 19:49:53 +0200

    small refactoring

--- first.cpp
+++ first.cpp
@@ -4,11 +4,9 @@
 
 #define MAX 500001
 
-using namespace std;
+std::vector<std::pair<int,int> > g[MAX];
 
-vector<pair<int,int> > g[MAX];
-
-int timeInNode[MAX], timeOutNode[MAX], dist[MAX];
+int timeInNode[MAX], timeOutNode[MAX], distanceToRoot[MAX];
 int up[MAX][20];
 
 int logarifm;
@@ -19,7 +17,7 @@ void dfs (int v, int p = 0, int lengthToRoot = 0)
     int i, to;
     timeInNode[v] = passedNodesCount++;
     up[v][0] = p;
-    dist[v] = lengthToRoot;
+    distanceToRoot[v] = lengthToRoot;
     
     for(i = 1; i <= logarifm; i++)
         up[v][i] = up[up[v][i-1]][i-1];
@@ -33,50 +31,49 @@ void dfs (int v, int p = 0, int lengthToRoot = 0)
     timeOutNode[v] = passedNodesCount++;
 }
 
-int Parent(int a, int b)
+int getParentNodeIndex(int u, int v)
 {
-    return (timeInNode[a] <= timeInNode[b]) && (timeOutNode[a] >= timeOutNode[b]);
+    return (timeInNode[u] <= timeInNode[v]) && (timeOutNode[u] >= timeOutNode[v]);
 }
 
-int LCA (int a, int b)
+int LCA(int u, int v)
 {
-    if (Parent(a, b)) return a;
-    if (Parent(b, a)) return b;
+    if (getParentNodeIndex(u, v)) return u;
+    if (getParentNodeIndex(v, u)) return v;
     
     for (int i = logarifm; i >= 0; i--)
-        if (!Parent(up[a][i], b)) a = up[a][i];
+        if (!getParentNodeIndex(up[u][i], v)) u = up[u][i];
     
-    return up[a][0];
+    return up[u][0];
 }
 
 int main()
 {
-    int n;
-    cin >> n;
+    int nodesCount;
+    std::cin >> nodesCount;
     
-    logarifm = log(n);
-    
-    for(int i = 0; i < n - 1; i++)
+    for(int i = 0; i < nodesCount - 1; i++)
     {
         int u, v, w;
-        cin >> u >> v >> w;
+        std::cin >> u >> v >> w;
         
-        g[u].push_back(make_pair(v,w));
-        g[v].push_back(make_pair(u,w));
+        g[u].push_back(std::make_pair(v,w));
+        g[v].push_back(std::make_pair(u,w));
     }
     
+    logarifm = log(nodesCount);
     dfs(0);
     
     int questionsCount;
-    cin >> questionsCount;
+    std::cin >> questionsCount;
     for(int i = 0; i < questionsCount; i++)
     {
         int u, v;
-        cin >> u >> v;
+        std::cin >> u >> v;
         
         int lca = LCA(u, v);
-        int res = dist[u] - dist[lca] + dist[v] - dist[lca];
-        cout << res << endl;
+        int res = distanceToRoot[u] - distanceToRoot[lca] + distanceToRoot[v] - distanceToRoot[lca];
+        std::cout << res << std::endl;
     }
     return 0;
 }
