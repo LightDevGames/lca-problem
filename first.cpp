27f1071172b922880c73b31edecabb8211ffa366
Yehor Bilenko <lightdevgames@gmail.com>
2020-03-18 20:30:13 +0200

    fully working

--- first.cpp
+++ first.cpp
@@ -2,8 +2,6 @@
 #include <vector>
 #include "math.h"
 
-#define MAX 500001
-
 struct ToDo
 {
     int adjacentVertex;
@@ -16,27 +14,26 @@ struct ToDo
     }
 };
 
-std::vector<std::vector<ToDo>> graphAdjencyList(MAX);
+std::vector<std::vector<ToDo>> graphAdjencyList;
+std::vector<std::vector<int>> logarithmUp;
+std::vector<int> distanceToRoot;
+std::vector<int> timeInNode;
+std::vector<int> timeOutNode;
 
-std::vector<int> timeInNode(MAX);
-std::vector<int> timeOutNode(MAX);
-std::vector<int> distanceToRoot(MAX);
-
-std::vector<std::vector<int>> up(MAX);
-
-int logarithm;
+int nodesCount;
+int logarithm = 1;
 
 int passedNodesCount = 0;
 void dfs (int vertex, int parent, int lengthToRoot)
 {
     timeInNode[vertex] = passedNodesCount;
     passedNodesCount++;
-    up[vertex][0] = parent;
+    logarithmUp[vertex][0] = parent;
     distanceToRoot[vertex] = lengthToRoot;
     
     for(int i = 1; i <= logarithm; i++)
     {
-        up[vertex][i] = up[up[vertex][i-1]][i-1];
+        logarithmUp[vertex][i] = logarithmUp[logarithmUp[vertex][i-1]][i-1];
     }
     
     for(int i = 0; i < graphAdjencyList[vertex].size(); i++)
@@ -52,32 +49,48 @@ void dfs (int vertex, int parent, int lengthToRoot)
     passedNodesCount++;
 }
 
-int getParentNodeIndex(int u, int v)
+int isParent(int parentVertex, int vertex)
 {
-    return (timeInNode[u] <= timeInNode[v]) && (timeOutNode[u] >= timeOutNode[v]);
+    return (timeInNode[parentVertex] <= timeInNode[vertex]) && (timeOutNode[parentVertex] >= timeOutNode[vertex]);
 }
 
 int LCA(int u, int v)
 {
-    if (getParentNodeIndex(u, v)) return u;
-    if (getParentNodeIndex(v, u)) return v;
+    if (isParent(u, v)) return u;
+    if (isParent(v, u)) return v;
     
     for (int i = logarithm; i >= 0; i--)
-        if (!getParentNodeIndex(up[u][i], v)) u = up[u][i];
-    
-    return up[u][0];
-}
-
-int main()
-{
-    int nodesCount;
-    std::cin >> nodesCount;
-    
-    for(int i = 0; i < up.size(); i++)
     {
-        up[i].resize(20);
+        if (!isParent(logarithmUp[u][i], v))
+        {
+            u = logarithmUp[u][i];
+        }
     }
     
+    return logarithmUp[u][0];
+}
+
+void resizeVectors()
+{
+    while ((1 << logarithm) <= nodesCount)
+    {
+        logarithm++;
+    }
+
+    graphAdjencyList.resize(nodesCount);
+    distanceToRoot.resize(nodesCount);
+    timeInNode.resize(nodesCount * 2);
+    timeOutNode.resize(nodesCount * 2);
+    
+    logarithmUp.resize(nodesCount + 2);
+    for(int i = 0; i < logarithmUp.size(); i++)
+    {
+        logarithmUp[i].resize(logarithm + 2);
+    }
+}
+
+void readData()
+{
     for(int i = 0; i < nodesCount - 1; i++)
     {
         int u, v, w;
@@ -88,10 +101,10 @@ int main()
         graphAdjencyList[u].push_back(t1);
         graphAdjencyList[v].push_back(t2);
     }
-    
-    logarithm = log(nodesCount);
-    dfs(0, 0, 0);
-    
+}
+
+void answearQuestions()
+{
     int questionsCount;
     std::cin >> questionsCount;
     for(int i = 0; i < questionsCount; i++)
@@ -103,5 +116,16 @@ int main()
         int res = distanceToRoot[u] - distanceToRoot[lca] + distanceToRoot[v] - distanceToRoot[lca];
         std::cout << res << std::endl;
     }
+}
+
+int main()
+{
+    std::cin >> nodesCount;
+    
+    resizeVectors();
+    readData();
+    dfs(0, 0, 0);
+    answearQuestions();
+    
     return 0;
 }
